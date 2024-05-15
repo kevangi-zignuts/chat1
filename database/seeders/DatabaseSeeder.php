a4c9475aabedf9c2828f9a89308a6e057ffcc868
kevangi-zignuts <kevangip@zignuts.com>
2024-05-15 15:07:21 +0530

    Add a Login and Registration functionality (for that add controller, blade file and there route)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $users = [
+            [
+                'name' => 'Test1',
+                'email' => 'test1@example.com',
+                'password' => Hash::make('password'),
+            ],
+            [
+                'name' => 'Test2',
+                'email' => 'test2@example.com',
+                'password' => Hash::make('password'),
+            ],
+            [
+                'name' => 'Test3',
+                'email' => 'test3@example.com',
+                'password' => Hash::make('password'),
+            ],
+        ];
+        foreach ($users as $user) {
+            User::updateOrCreate(
+                [
+                    'email' => $user['email'],
+                ],
+                [
+                    'name' => $user['name'],
+                    'password' => $user['password'],
+                ]
+            );
+        }
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::whereNotIn('email', array_column($users, 'email'))->delete();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\auth\LoginController;
+use App\Http\Controllers\auth\RegistrationController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [LoginController::class, 'form'])->name('login');
+Route::post('/login', [LoginController::class, 'login'])->name('auth.login');
+Route::get('/register-form', [RegistrationController::class, 'form'])->name('auth.registration-form');
+Route::post('/register', [RegistrationController::class, 'register'])->name('auth.register');
+Route::get('/logout', [LoginController::class, 'logout'])->name('auth.logout');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware('auth:sanctum')->name('dashboard');
+
+
+// Route::get('/dashboard', [MessageController::class, 'index'])->middleware('auth:sanctum')->name('dashboard');
